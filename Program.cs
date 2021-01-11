71b0c4cc94115a4ceb702fc10ace80dec6dcb3bb
Dhar Patel <dharwlu@gmail.com>
2021-01-11 10:42:21 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("MLH Local Hack Day: Build 2021");
+            var name = "Dhar";
+            Console.WriteLine($"Hi im {name}");
+        }
+    }
+}
